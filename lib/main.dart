eb885166d002757ba91908275f93244cc4d9ba5d
andresperezfc <felipe3apc@gmail.com>
2022-01-31 15:30:58 -0500

    Configuración Firebase - Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyBA9GAzGPkPNzrejb6NCQun3XzbQeMT3nM',
+      appId: '1:828967822835:web:ad9eac94c604ac94b3de86',
+      messagingSenderId: '828967822835',
+      projectId: 'instagram-clone-63fff',
+      storageBucket: 'instagram-clone-63fff.appspot.com',
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
