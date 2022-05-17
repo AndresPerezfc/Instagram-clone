56d2cc476f7046b097f645d9265c0aa1eec56ff2
andresperezfc <felipe3apc@gmail.com>
2022-05-16 22:42:07 -0500

    Signup Screen UI (Mobile)

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/text_field_input.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            SvgPicture.asset(
+              'assets/ic_instagram.svg',
+              color: primaryColor,
+              height: 64,
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            //Circular widget para acetpar y seleccionar una imagen de perfil
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage('assets/perfil.jpeg'),
+                ),
+                Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                        )))
+              ],
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            TextFieldInput(
+              textEditingController: _usernameController,
+              hintText: 'Ingresar tu nombre de usuario',
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            TextFieldInput(
+                textEditingController: _emailController,
+                hintText: 'Ingresar Correo electrónico',
+                textInputType: TextInputType.emailAddress),
+            const SizedBox(
+              height: 24,
+            ),
+            TextFieldInput(
+              textEditingController: _passwordController,
+              hintText: 'Ingresar Contraseña',
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            TextFieldInput(
+              textEditingController: _bioController,
+              hintText: 'Ingresar tu biografía',
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            InkWell(
+              child: Container(
+                child: const Text("Crear cuenta"),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                    ),
+                    color: blueColor),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: const Text("Ya eres usuario?"),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: const Text(
+                      "Inicia sesión",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
